7617ef9977bef538e1ff238c66bcace7c4c8d749
Brian Wood <bcwood79@yahoo.com>
2022-04-24 13:25:09 -0400

    Added first frame

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from tkinter import *
 
+# Override the settings of the window
 root = Tk()
 root.configure(bg='black')
 root.geometry('1140x720')
-root.title('Minesweeper Game')
+root.title('Minesweeper Game')  # FIXME This is not showing up; check other machines
 root.resizable(False, False)
 
+top_frame = Frame(
+    root,
+    bg = 'red', # FIXME change to black later
+    width = '1440',
+    height = '180'
+)
+top_frame.place(x=0, y=0)
+
+# Run the window
 root.mainloop()
