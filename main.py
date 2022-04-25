a04d9a60070a0ab8304c74035c9996ddb0b448f7
Brian Wood <bcwood79@yahoo.com>
2022-04-25 00:13:11 -0400

    Add settings file
    Update main with constants from settings

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from tkinter import *
+import settings
 
 # Override the settings of the window
 root = Tk()
 root.configure(bg='black')
-root.geometry('1140x720')
+root.geometry(f"{settings.WIDTH}x{settings.HEIGHT}")
 root.title('Minesweeper Game')  # FIXME This is not showing up; check other machines
 root.resizable(False, False)
 
@@ -13,7 +14,15 @@ top_frame = Frame(
     width = '1440',
     height = '180'
 )
-top_frame.place(x=0, y=0)
+top_frame.place(x = 0, y = 0)
+
+left_frame = Frame(
+    root,
+    bg = 'blue', # FIXME change to black later
+    width = '360',
+    height = '540'
+)
+left_frame.place(x = 0, y = 180)
 
 # Run the window
 root.mainloop()
