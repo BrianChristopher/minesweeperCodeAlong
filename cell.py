0b7f0a68ea91b29c1113349a37b41b36dff63344
Brian Wood <bcwood79@yahoo.com>
2022-05-03 12:48:18 -0400

    Changes from Chromebook

--- cell.py
+++ cell.py
@@ -1,14 +1,18 @@
 from tkinter import Button
 
 class Cell:
-    def __init__(self, isMine = False):
+    def __init__(self, x, y, isMine = False):
+        self.x = x
+        self.y = y
         self.isMine = isMine
         self.cell_btn_object = None
 
     def create_btn_object(self, location):
         btn = Button(
             location,
-            text="Text"
+            width=12,
+            height=4,
+            text=f"{self.x},{self.y}"
         )
         btn.bind('<Button-1>', self.left_click_actions)  # Left click
         btn.bind('<Button-3>', self.right_click_actions) # Right click
